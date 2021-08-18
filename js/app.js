b59c4c4a8781fe938bb9b267cb82b4e61021a548
nayeem407 <sabbirahmednayeem941@gmail.com>
2021-08-18 20:26:09 +0600

    java script commit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+function getPin(){
+    const pin = Math.round(Math.random() *10000);
+    const pinString = pin + '';
+    if(pinString.length == 4){
+        return pin;
+
+    }
+    else{
+        // console.log('got 3 digit and calling again',pin)
+        return getPin()
+    }
+     
+}
+
+
+
+
+function generatePin(){
+    const pin = getPin();
+
+    document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('key-pad').addEventListener('click', function(event){
+    const number =(event.target.innerText );
+    const calcInput = document.getElementById('typed-numbers');
+    if(isNaN (number)){
+        // console.log(number)
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+    }
+    else{
+        const previousNumber = calcInput .value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber
+    
+
+    }
+   
+})
+function veryfypin(){
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value
+    const successMassage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail')
+    if(pin == typedNumbers){
+         successMassage.style.display= 'block'
+         failError.style.display='none';
+    }
+    else{
+       successMassage.style.display= 'none'
+       failError.style.display = 'block'
+
+        
+    }
+}
